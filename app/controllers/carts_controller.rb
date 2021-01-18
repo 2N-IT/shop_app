1ab9a66e3976e181db610d1dc774505dd66baa17
krzykamil <krzysiekkamilpiotrek@gmail.com>
2021-01-18 17:34:03 +0100

    cart 2

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,28 +1,31 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  def update
-    product_in_cart = cart.product_items.find_by(product_id: params[:cart][:product_id])
-    if product_in_cart
-      product_in_cart.update(quantity: params[:cart][:quantity])
-    else
-      ProductItem.create(
-        cart: cart,
-        product: product,
-        name: product.name,
-        price: product.price,
-        quantity: params[:cart][:quantity]
-      )
-    end
-    head :no_content
+def update
+  product_in_cart = cart.product_items.find_by(product_id: params[:cart][:product_id])
+  new_quantity = params[:cart][:quantity].to_i
+  if product_in_cart
+    new_quantity.zero? ? product_in_cart.destroy : product_in_cart.update(quantity: new_quantity)
+  else
+    ProductItem.create(
+      cart: cart,
+      product: product,
+      name: product.name,
+      price: product.price,
+      quantity: new_quantity
+    )
   end
+  head :no_content
+end
 
   def destroy
     cart.product_items.destroy_all
     redirect_to products_path
   end
 
-  def show; end
+  def show
+
+  end
 
   private
 
